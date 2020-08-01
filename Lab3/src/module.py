7af20660221236134c7b65e8562dfdfc7b563fa0
WDZRMPCBIT <WDZRMPCBIT@163.com>
2020-08-02 00:46:49 +0800

    add module

--- Lab3/src/module.py
+++ Lab3/src/module.py
@@ -22,14 +22,14 @@ class ESIM(torch.nn.Module):
             hidden_dim * 8, hidden_dim, hidden_dim, dropout)
         self.out = layer.OutputLayer(hidden_dim * 8, hidden_dim, result_num)
 
-    def forward(self, p, p_length, h, h_length):
+    def forward(self, p, h):
         p_embeded = self.embedder(p)
         h_embeded = self.embedder(h)
 
         p_ = self.encoder(p_embeded)
         h_ = self.encoder(h_embeded)
 
-        p_mask, h_mask = (p != 1).long(), (h != 1).long()
+        p_mask, h_mask = (p != 0).long(), (h != 0).long()
         m_p, m_h = self.inference(p_, h_, p_mask, h_mask)
 
         v = self.inference_composition(m_p, m_h, p_mask, h_mask)
