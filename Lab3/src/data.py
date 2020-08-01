e8d63090cfbcca14c6a0aee58bc11981cca04d09
WDZRMPCBIT <WDZRMPCBIT@163.com>
2020-08-01 23:28:59 +0800

    add data part

---
+++ Lab3/src/data.py
@@ -0,0 +1,74 @@
+def load_data(path, size=None):
+    import pandas as pd
+    data = pd.read_table(path)
+
+    return data['sentence1'][:size], data['sentence2'][:size], data[
+        'gold_label'][:size]
+
+
+def load_vocabulary(path):
+    vocabulary = {}
+
+    cnt = 0
+    file = open(path, 'r')
+    for line in file:
+        vocabulary[line.strip()] = cnt
+        cnt = cnt + 1
+
+    return vocabulary
+
+
+def load_embedding(path, dim):
+    ret = []
+
+    file = open(path, 'r')
+    for line in file:
+        ret.append([float(s) for s in line.split()])
+
+    return ret
+
+
+def tokenizer(sentence, vocabulary):
+    import re
+    ret = []
+    max_length = 0
+
+    for s in sentence:
+        vector = []
+        words = re.split(r'[-\";,.!?\.\s]', str(s))
+        for word in words:
+            w = word.rstrip('\'').rstrip('\'s').lower()
+            if w in vocabulary.keys():
+                vector.append(vocabulary[w])
+
+        max_length = max(max_length, len(vector))
+        ret.append(vector)
+
+    return ret, max_length
+
+
+def padding(sentence, max_length):
+    ret = []
+
+    for s in sentence:
+        tmp = s[:max_length]
+        for i in range(max_length - len(tmp)):
+            tmp.append(0)
+        ret.append(tmp)
+
+    return ret
+
+
+'''
+if __name__ == '__main__':
+    train_sentence1, train_sentence2, train_label = load_data(
+        '../test/snli_1.0_train.txt', 5)
+    test_sentence1, test_sentence2, ttest_label = load_data(
+        '../test/snli_1.0_test.txt', 1)
+
+    vocabulary = load_vocabulary('../data/vocabulary.txt')
+
+    tmp, max_length = tokenizer(train_sentence1, vocabulary)
+
+    tmp = padding(tmp, max_length)
+'''
---
+++ Lab3/src/pretreat.py
@@ -0,0 +1,86 @@
+from data import load_data
+
+DIM = 50
+
+
+def make_vocabulary(sentence, vec_path):
+    import re
+
+    vocabulary = []
+    for s in sentence:
+        words = str(s)
+        words = re.split(r'[-\";,.!?\.\s]', words)
+        vocabulary = vocabulary + words
+    for i in range(len(vocabulary)):
+        vocabulary[i] = vocabulary[i].rstrip('\'').rstrip('\'s').lower()
+    vocabulary = set(vocabulary)
+
+    final_vocabulary = [0]
+
+    vec_file = open(vec_path, 'r', encoding='utf-8')
+    for line in vec_file:
+        word = line.split()
+        if word[0] in vocabulary:
+            final_vocabulary.append(word[0])
+    vec_file.close()
+
+    file = open('../data/vocabulary.txt', 'w')
+    for word in final_vocabulary:
+        file.write(str(word) + '\n')
+    file.close()
+
+
+def make_vector(vocabulary, vec_file, out_file):
+    match = {}
+
+    num = 0
+    keys = vocabulary.keys()
+    length = len(vocabulary)
+
+    file = open(vec_file, 'r', encoding='utf-8')
+    for line in file:
+        word = line.split()
+
+        if word[0] in keys:
+            vec = word[1:].copy()
+            match[vocabulary[word[0]]] = vec
+            num = num + 1
+
+        if num == length:
+            break
+    file.close()
+
+    match[0] = [0.0] * DIM
+
+    file = open(out_file, 'w')
+    for i in range(length):
+        if i in match.keys():
+            for j in range(DIM):
+                file.write(str(match[i][j]) + ' ')
+        file.write('\n')
+    file.close()
+
+
+if __name__ == '__main__':
+    train_sentence1, train_sentence2, train_label = load_data(
+        '../test/snli_1.0_train.txt', 50000)
+    test_sentence1, test_sentence2, ttest_label = load_data(
+        '../test/snli_1.0_test.txt')
+
+    make_vocabulary(
+        train_sentence1 + train_sentence2 + test_sentence1 + test_sentence2,
+        '../data/glove.6B.50d.txt')
+
+    file = open('../data/vocabulary.txt', 'r')
+    vocabulary = {}
+    cnt = 0
+    for line in file:
+        if cnt == 0:
+            cnt = cnt + 1
+            continue
+        vocabulary[line.strip()] = cnt
+        cnt = cnt + 1
+    file.close()
+
+    make_vector(vocabulary, '../data/glove.6B.50d.txt',
+                '../data/word2vec.txt')
