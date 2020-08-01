784e46598beb6003ca7e08a71f158eb7ae4e68a8
WDZRMPCBIT <WDZRMPCBIT@163.com>
2020-08-02 03:04:00 +0800

    change data and epoch limit

--- Lab3/src/main.py
+++ Lab3/src/main.py
@@ -2,9 +2,16 @@ from train import train
 from test import test
 
 if __name__ == '__main__':
-    nn = train('../test/snli_1.0_train.txt', '../data/vocabulary.txt',
-               '../data/word2vec.txt', '../model/esim.pkl')
-    result = test(nn, '../test/snli_1.0_test.txt', '../data/vocabulary.txt',
-                  '../data/word2vec.txt')
+    nn = train('../test/snli_1.0_train.txt',
+               '../data/vocabulary.txt',
+               '../data/word2vec.txt',
+               '../model/esim.pkl',
+               data_size=10000,
+               epoch=20)
+    result = test(nn,
+                  '../test/snli_1.0_test.txt',
+                  '../data/vocabulary.txt',
+                  '../data/word2vec.txt',
+                  data_size=3000)
 
     print(result)
--- Lab3/src/test.py
+++ Lab3/src/test.py
@@ -9,22 +9,26 @@ def test(
     test_data_path,
     vocabulary_path,
     embedding_matrix_path,
+    data_size=None
 ):
-    sentence1, sentence2, label = data.load_data(test_data_path)
+    sentence1, sentence2, label = data.load_data(test_data_path, data_size)
 
     vocabulary = data.load_vocabulary(vocabulary_path)
 
-    sentence1 = data.tokenizer(sentence1, vocabulary)
-    sentence2 = data.tokenizer(sentence2, vocabulary)
-    sentence1 = data.padding(sentence1)
-    sentence2 = data.padding(sentence2)
+    sentence1, max_length1 = data.tokenizer(sentence1, vocabulary)
+    sentence2, max_length2 = data.tokenizer(sentence2, vocabulary)
+    max_length = max(max_length1, max_length2)
+
+    sentence1 = data.padding(sentence1, max_length)
+    sentence2 = data.padding(sentence2, max_length)
     sentence1 = data.list2torch(sentence1)
     sentence2 = data.list2torch(sentence2)
 
     label = data.label2num(label)
-    label = np.numpy(label)
+    label = np.array(label)
 
-    predicate_label = torch.max(esim(sentence1, sentence2), 1)[1]
+    _, out = esim(sentence1, sentence2)
+    predicate_label = torch.max(out, 1)[1]
     predicate_label = predicate_label.numpy()
 
     return len(np.argwhere(label != predicate_label)) / len(label)
