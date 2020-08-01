a1137de99fcd4ef183b2cfbb922e32e91ad8c8ee
WDZRMPCBIT <WDZRMPCBIT@163.com>
2020-08-02 01:21:49 +0800

    add main

---
+++ Lab3/src/main.py
@@ -0,0 +1,10 @@
+from train import train
+from test import test
+
+if __name__ == '__main__':
+    nn = train('../test/snli_1.0_train.txt', '../data/vocabulary.txt',
+               '../data/word2vec.txt')
+    result = test(nn, '../test/snli_1.0_test.txt', '../data/vocabulary.txt',
+                  '../data/word2vec.txt')
+
+    print(result)
