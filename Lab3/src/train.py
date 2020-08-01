e380a5e7e378346759cbaae321e0a6e1f8d408b1
WDZRMPCBIT <WDZRMPCBIT@163.com>
2020-08-02 01:17:30 +0800

    add train

--- Lab3/src/train.py
+++ Lab3/src/train.py
@@ -1,22 +1,52 @@
 import torch
 import data
 from module import ESIM
+import torch.utils.data as Data
 
-if __name__ == '__main__':
-    train_sentence1, train_sentence2, train_label = data.load_data(
-        '../test/snli_1.0_train.txt', 5)
-    test_sentence1, test_sentence2, ttest_label = data.load_data(
-        '../test/snli_1.0_test.txt', 1)
 
-    vocabulary = data.load_vocabulary('../data/vocabulary.txt')
+def train(train_data_path,
+          vocabulary_path,
+          embedding_matrix_path,
+          data_size,
+          hidden_dim=50,
+          batch_size=1000,
+          lr=0.2,
+          epoch=30):
+    sentence1, sentence2, label = data.load_data(train_data_path, data_size)
 
-    embedding_matrix = data.load_embedding('../data/word2vec.txt')
+    vocabulary = data.load_vocabulary(vocabulary_path)
+
+    embedding_matrix = data.load_embedding(embedding_matrix_path)
     embedding_matrix = data.list2torch(embedding_matrix)
 
-    train_sentence1 = data.tokenizer(train_sentence1, vocabulary)
-    train_sentence2 = data.tokenizer(train_sentence2, vocabulary)
-    train_sentence1 = data.padding(train_sentence1)
-    train_sentence2 = data.padding(train_sentence2)
+    sentence1 = data.tokenizer(sentence1, vocabulary)
+    sentence2 = data.tokenizer(sentence2, vocabulary)
+    sentence1 = data.padding(sentence1)
+    sentence2 = data.padding(sentence2)
+    sentence1 = data.list2torch(sentence1)
+    sentence2 = data.list2torch(sentence2)
 
-    train_sentence1 = data.list2torch(train_sentence1)
-    train_sentence2 = data.list2torch(train_sentence2)
+    label = data.label2num(label)
+    label = data.list2torch(label)
+
+    torch_dataset = Data.TensorDataset(sentence1, sentence2, label)
+    loader = Data.DataLoader(dataset=torch_dataset,
+                             batch_size=batch_size,
+                             shuffle=True,
+                             num_workers=2)
+
+    esim = ESIM(hidden_dim, len(vocabulary), embedding_matrix,
+                embedding_matrix.size(1))
+    optimizer = torch.optim.Adam(esim.parameters(), lr=lr)
+    loss_func = torch.nn.CrossEntropyLoss()
+
+    for e in range(epoch):
+        for step, (s1, s2, label) in enumerate(loader):
+            _, out = esim(s1, s2)
+            loss = loss_func(out, label)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+    return esim
