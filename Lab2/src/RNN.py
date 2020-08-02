6d1b436a823a2f748d1c13ba9564ae44bdc2b1fb
WDZRMPCBIT <WDZRMPCBIT@163.com>
2020-08-03 01:31:36 +0800

    replace with biLSTM

--- Lab2/src/RNN.py
+++ Lab2/src/RNN.py
@@ -4,9 +4,12 @@ import torch
 import torch.nn.functional as function
 import torch.utils.data as Data
 
-BATCH_SIZE = 1000
+BATCH_SIZE = 1024
 EPOCH = 20
-LR = 0.2
+LR = 0.02
+DATA_SIZE = None
+EMBEDDING_DIM = 64
+HIDDEN_SIZE = 1024
 
 
 def build_vocabulary(phrase):
@@ -51,31 +54,22 @@ class RNN(torch.nn.Module):
         super(RNN, self).__init__()
 
         self.embedding = torch.nn.Embedding(n_feature, embedding_dim)
-
-        self.hidden = torch.nn.LSTM(embedding_dim, n_hidden, batch_first=True)
-
-        self.out = torch.nn.Linear(n_hidden, n_output)
+        self.dropout = torch.nn.Dropout(0.5)
+        self.lstm = torch.nn.LSTM(embedding_dim, n_hidden, bidirectional=True)
+        self.out = torch.nn.Linear(2 * n_hidden, n_output)
 
     def forward(self, x):
-        length = []
-        for s in x:
-            for i in range(len(s)):
-                if (s[i] == 0):
-                    length.append(i - 1)
-                    break
-
         x = self.embedding(x)
 
-        x, _ = self.hidden(x)
+        x = x.permute(1, 0, 2)
 
-        for i in range(len(x)):
-            current = x[i][length[i]].unsqueeze(0)
-            if (i == 0):
-                t = current
-            else:
-                t = torch.cat((t, current), dim=0)
+        x, (h_n, c_n) = self.lstm(x)
 
-        x = self.out(t)
+        h_n = self.dropout(h_n)
+
+        x = torch.cat([h_n[i, :, :] for i in range(h_n.shape[0])], dim=1)
+
+        x = self.out(x)
 
         return function.softmax(x, dim=1)
 
@@ -84,8 +78,8 @@ if __name__ == '__main__':
     train_data = pd.read_csv('../test/train.tsv', sep='\t')
     test_data = pd.read_csv('../test/test.tsv', sep='\t')
 
-    train_phrase = list(train_data['Phrase'][:1000])
-    train_sentiment = list(train_data['Sentiment'][:1000])
+    train_phrase = list(train_data['Phrase'][:DATA_SIZE])
+    train_sentiment = list(train_data['Sentiment'][:DATA_SIZE])
     test_phrase = list(test_data['Phrase'])
 
     vocabulary = build_vocabulary(train_phrase + test_phrase)
@@ -102,7 +96,7 @@ if __name__ == '__main__':
     train_y = torch.from_numpy(np.array(train_sentiment)).type(
         torch.LongTensor)
 
-    rnn = RNN(len(vocabulary) + 1, 16, 40, 5)
+    rnn = RNN(len(vocabulary) + 1, EMBEDDING_DIM, HIDDEN_SIZE, 5)
     optimizer = torch.optim.Adam(rnn.parameters(), lr=LR)
     loss_func = torch.nn.CrossEntropyLoss()
 
@@ -115,6 +109,7 @@ if __name__ == '__main__':
     for epoch in range(EPOCH):
         for step, (batch_x, batch_y) in enumerate(loader):
             out = rnn(batch_x)
+
             loss = loss_func(out, batch_y)
 
             optimizer.zero_grad()
