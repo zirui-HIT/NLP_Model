6f5b183cd1d09035a1da787a6183ddf3ff18703b
WDZRMPCBIT <WDZRMPCBIT@163.com>
2020-08-02 11:56:42 +0800

    reset parameter of train and test

--- Lab3/src/main.py
+++ Lab3/src/main.py
@@ -6,12 +6,12 @@ if __name__ == '__main__':
                '../data/vocabulary.txt',
                '../data/word2vec.txt',
                '../model/esim.pkl',
-               data_size=10000,
-               epoch=20)
+               lr=0.02,
+               hidden_dim=300,
+               batch_size=2048)
     result = test(nn,
                   '../test/snli_1.0_test.txt',
                   '../data/vocabulary.txt',
-                  '../data/word2vec.txt',
-                  data_size=3000)
+                  '../data/word2vec.txt')
 
     print(result)
