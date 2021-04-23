e07dfc0fb4eff84688f38e1f37a7eedb27c6a032
WDZRMPCBIT <WDZRMPCBIT@163.com>
2021-04-23 18:52:14 +0800

    implemet processor

--- fastText/src/processor.py
+++ fastText/src/processor.py
@@ -3,47 +3,99 @@ import numpy as np
 from data import Vocabulary, DataManager
 from copy import deepcopy
 from typing import List
+from tqdm import tqdm
 
 
 class Processor(object):
-    def __init__(self, vocabulary: Vocabulary, model):
+    def __init__(self, vocabulary: Vocabulary, batch_size: int, shuffle: bool):
         self._vocabulary = deepcopy(vocabulary)
-        self._model = deepcopy(model)
+        self._batch_size = batch_size
+        self._shuffle = shuffle
 
-    def fit(self, data: DataManager) -> float:
+        # TODO
+        self._loss = None
+        self._model = None
+        self._optimizer = None
+
+    def fit(self, path: str, train_data: DataManager, valid_data: DataManager = None) -> float:
         """train model with data
 
         train model with train_data and update model by the accuracy of it
 
         Args:
-            data: train data, which contains sentence and label
+            train_data: train data, which contains sentence and label
+            valid_data: data used to select best model
+            path: path to save model
 
         Returns:
             accuracy on train data
         """
-        pass
+        self._model.train()
+        if valid_data is None:
+            valid_data = deepcopy(train_data)
+
+        best_accuracy = 0
+        package = train_data.package(self._batch_size, self._shuffle)
+        for current_sentences, current_labels in tqdm(package, ncols=len(package)):
+            sentences, mask = self._wrap_sentence(current_sentences)
+            labels = torch.Tensor(current_labels, dtype=torch.LongTensor)
+
+            predict_labels = self._model(sentences, mask)
+            loss = self._loss_function(predict_labels, labels)
+
+            self._optimizer.zero_grad()
+            loss.backward()
+            self._optimizer.step()
+
+            if self.validate(valid_data) > best_accuracy:
+                self.dump(path)
 
     def validate(self, data: DataManager) -> float:
         """validate model
 
+        calculate accuracy on validation data with processor model
+
         Args:
-            data: validate data
+            data: validate data that contains sentences and labels
 
         Returns:
             accuracy on validation data
         """
-        pass
+        actual_labels = data.labels()
+        predict_labels = self.predict(data)
+
+        if len(actual_labels) != len(predict_labels):
+            raise Exception("inequality length of actual and predict labels")
+
+        count = 0
+        length = len(actual_labels)
+        for i in range(length):
+            if actual_labels[i] == predict_labels[i]:
+                count += 1
+
+        return count / length
 
     def predict(self, data: DataManager) -> List[int]:
         """predict label
 
         Args:
-            data: test data
+            data: test data that only contains sentences
 
         Returns:
             label of every data
         """
-        pass
+        self._model.eval()
+
+        package = data.package(self._batch_size, False)
+        result_labels = []
+        for current_sentences in tqdm(package, ncols=len(package)):
+            sentences, mask = self._wrap_sentence(current_sentences)
+
+            predict_labels = self._model(sentences, mask)
+            _, predict_labels = predict_labels.topk(1, dim=1)
+            result_labels.extend(predict_labels)
+
+        return result_labels
 
     def load(self, path: str):
         """load model and vocabulary from given path
@@ -62,6 +114,15 @@ class Processor(object):
         """
         torch.save(self._model.state_dict(), path)
         self._vocabulary.dump(path)
-    
+
     def _wrap_sentence(self, sentences: List[List[str]]) -> List[List[int]]:
-        
+        indexes = [[self._vocabulary.get(w) for w in s] for s in sentences]
+        length = len(max(indexes, key=len))
+        pad_index = self._vocabulary.get('[PAD]')
+
+        for i in range(indexes):
+            indexes[i] = indexes[i] + \
+                [pad_index for i in range((length - len(indexes)))]
+        mask = torch.tensor(np.where(indexes != pad_index, 1))
+
+        return torch.Tensor(indexes, dtype=torch.LongTensor)
