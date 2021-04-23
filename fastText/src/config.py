f2e356841e5c75bbabc6ba8a8774113e018bd041
WDZRMPCBIT <WDZRMPCBIT@163.com>
2021-04-23 19:16:36 +0800

    remanage order

--- fastText/src/config.py
+++ fastText/src/config.py
@@ -2,19 +2,49 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
+# general argument
 parser.add_argument('--random_seed',
                     '-rs',
                     type=int,
                     default=0,
                     help="random seed")
-parser.add_argument('--mode', '-m', type=str, default='train',
+parser.add_argument('--mode',
+                    '-m',
+                    type=str,
+                    default='train',
                     help="train model or predict result")
 parser.add_argument('--model_path',
                     '-mp',
                     type=str,
                     default='../model/fastText.pkl',
                     help="path to save model")
+parser.add_argument('--max_length',
+                    '-ml',
+                    type=int,
+                    default=None,
+                    help="max length to be processed")
+parser.add_argument('--epoch',
+                    '-e',
+                    type=int,
+                    default=20,
+                    help="epoch")
+parser.add_argument('--batch_size',
+                    '-bs',
+                    type=int,
+                    default=4,
+                    help="batch size")
+parser.add_argument('--learning_rate',
+                    '-lr',
+                    type=float,
+                    default=1e-5,
+                    help="learing rate")
+parser.add_argument('--dropout',
+                    '-d',
+                    type=float,
+                    default=0.3,
+                    help="dropout")
 
+# train mode argument
 parser.add_argument('--train_data_path',
                     '-tdp',
                     type=str,
@@ -26,6 +56,7 @@ parser.add_argument('--valid_data_path',
                     default='../../Data/Sentiment_Analysis_on_Movie_Reviews/valid.tsv',
                     help="path of valid data")
 
+# test mode argument
 parser.add_argument('--predict_data_path',
                     'pdp',
                     type=str,
@@ -36,24 +67,5 @@ parser.add_argument('--save_path',
                     type=str,
                     default='../../Data/Sentiment_Analysis_on_Movie_Reviews/result_fastText.txt',
                     help="output path")
-
-parser.add_argument('--max_length',
-                    '-ml',
-                    type=int,
-                    default=None,
-                    help="max length to be processed")
-
-parser.add_argument('--epoch', '-e', type=int, default=20, help="epoch")
-parser.add_argument('--batch_size',
-                    '-bs',
-                    type=int,
-                    default=4,
-                    help="batch size")
-parser.add_argument('--learning_rate',
-                    '-lr',
-                    type=float,
-                    default=1e-5,
-                    help="learing rate")
-parser.add_argument('--dropout', '-d', type=float, default=0.3, help="dropout")
 
 args = parser.parse_args()
